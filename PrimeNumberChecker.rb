e5bb5e4c78ac3f6b806d9546b2e03268f7108b2d
Sonny Kui <sonnykui@yahoo.com>
2013-02-22 19:14:00 -0500

    Change to function call

--- PrimeNumberChecker.rb
+++ PrimeNumberChecker.rb
@@ -1,16 +1,23 @@
-number = 15
-prime = true
-if number % 2 != 0
+# Check if a number is prime
+# returns true or false
 
-	for factors in 3..number-1
-		if number % factors == 0
-			prime = false
-			break
+def isPrime(number)
+
+	if number % 2 == 0
+		return false
+	end
+	
+	for factor in 3..number-1
+		if number % factor == 0
+			return false
 		end
 	end
+
+	return true
 end
 
-if prime
+number = 15
+if isPrime(number)
 	puts "prime"
 else
 	puts "not prime"
